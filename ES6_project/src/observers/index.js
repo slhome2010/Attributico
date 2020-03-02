d8e1555cd358a0c334e8b633963fca7fd9ee2f65
slhome2010 <slhome2010@mail.ru>
2020-03-02 20:52:17 +0600

    this. clearFilter

--- ES6_project/src/observers/index.js
+++ ES6_project/src/observers/index.js
@@ -6,29 +6,30 @@ import { ATTRIBUTE_SYNCRO_TREES } from "../constants/global";
 export default class Observer {
     constructor(store) {
         this.store = store;
-        this.clearFilter = this.clearFilter.bind(this);
-       // this.treeReload = this.treeReload.bind(this)
+        // this.clearFilter = this.clearFilter.bind(this);
+        // this.treeReload = this.treeReload.bind(this)
     }
 
     /* Clear Filter if tree reload */
-    clearFilter = (tree) => {
-         if (tree.isFilterActive()) {
-             tree.clearFilter();
+    clearFilter(tree) {        
+        if (tree.isFilterActive()) {  
+                     
+            tree.clearFilter();
 
-        $('input[name *= "search"]').val("");
-        $('span[id *= "matches"]').text("");
-        $("[id ^=loadImg]").hide();
-          }
+            $('input[name *= "search"]').val("");
+            $('span[id *= "matches"]').text("");
+            $("[id ^=loadImg]").hide();
+        }
     }
 
     treeReload() {
         let state = this.store.getState().dndReducer;
-        console.log(state.activeNode.key, state.sourceNode.key, state.targetNode.key);
+        
         $(ATTRIBUTE_SYNCRO_TREES).each(function (indx, element) {
             let tree = $("#" + element.id).fancytree("getTree");
             tree.options.source.data.cache = $('input[name = "attributico_cache"]:checkbox').is(":checked");
             if ((tree !== state.tree) || state.selfReload) { // not reload active tree
-                this.clearFilter(tree).bind(this);
+                this.clearFilter(tree);
                 tree.reload().done(function () {
                     /* Зачем нужен флаг, что дерево в процессе перезагрузки?  */
                     tree.options.source.data.isPending = false;
@@ -41,11 +42,11 @@ export default class Observer {
                     }
                 });
             }
-        });
+        }.bind(this));
         /* Если активное дерево не перезагружалось, то надо установить активный узел принудительно */
         if (!state.selfReload) {
-            state.activeNode.setActive();           
-        }        
+            state.activeNode.setActive();
+        }
     }
 
     init() {
