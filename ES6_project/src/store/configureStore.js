afd4c3870ff3334db6fdf597a2dace5bc4883ffe
slhome2010 <slhome2010@mail.ru>
2020-04-03 17:10:50 +0600

    new production without DevTools

--- ES6_project/src/store/configureStore.js
+++ ES6_project/src/store/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
 
 export default function configureStore(reducer, initialState, enhance) {
-  return createStore(reducer, initialState, enhance());
+  return createStore(reducer, initialState);
 }
