fc0377091be6f443f914f597698dbed52cfa00cd
slhome2010 <slhome2010@mail.ru>
2020-08-12 12:26:31 +0600

    fancyFilter init and manage erorrs fixed

--- ES6_project/src/components/Trees/AttributeGroupTree.js
+++ ES6_project/src/components/Trees/AttributeGroupTree.js
@@ -196,12 +196,12 @@ export default class AttributeGroupTree {
                 command.execute();
             },
             filter: {
-                autoApply: true, // Re-apply last filter if lazy data is loaded
-                counter: true, // Show a badge with number of matching child nodes near parent icons
-                fuzzy: false, // Match single characters in order, e.g. 'fb' will match 'FooBar'
-                hideExpandedCounter: true, // Hide counter badge, when parent is expanded
-                highlight: true, // Highlight matches by wrapping inside <mark> tags
-                mode: "dimm" // Grayout unmatched nodes (pass "hide" to remove unmatched node instead)
+                autoApply: $("#fs_" + this.currentTab + "_autoApply" + this.lng_id).is(":checked"), // Re-apply last filter if lazy data is loaded
+                counter: $("#fs_" + this.currentTab + "_counter" + this.lng_id).is(":checked"), // Show a badge with number of matching child nodes near parent icons
+                fuzzy: $("#fs_" + this.currentTab + "_fuzzy" + this.lng_id).is(":checked"), // Match single characters in order, e.g. 'fb' will match 'FooBar'
+                hideExpandedCounter: $("#fs_" + this.currentTab + "_hideExpandedCounter" + this.lng_id).is(":checked"), // Hide counter badge, when parent is expanded
+                highlight: $("#fs_" + this.currentTab + "_highlight" + this.lng_id).is(":checked"), // Highlight matches by wrapping inside <mark> tags
+                mode: $("#fs_" + this.currentTab + "_hideMode" + this.lng_id).is(":checked") ? "hide" : "dimm" // Grayout unmatched nodes (pass "hide" to remove unmatched node instead)
             },
             init: (event, data) => {
                 let filter = new Filter(this.currentTab, data.tree, this.lng_id);
--- ES6_project/src/components/Trees/AttributeProductTree.js
+++ ES6_project/src/components/Trees/AttributeProductTree.js
@@ -87,12 +87,12 @@ export default class AttributeProductTree {
                 command.execute();
             },
             filter: {
-                autoApply: true, // Re-apply last filter if lazy data is loaded
-                counter: true, // Show a badge with number of matching child nodes near parent icons
-                fuzzy: false, // Match single characters in order, e.g. 'fb' will match 'FooBar'
-                hideExpandedCounter: true, // Hide counter badge, when parent is expanded
-                highlight: true, // Highlight matches by wrapping inside <mark> tags
-                mode: "dimm" // Grayout unmatched nodes (pass "hide" to remove unmatched node instead)
+                autoApply: $("#fs_" + this.currentTab + "_autoApply" + this.lng_id).is(":checked"),
+                counter: $("#fs_" + this.currentTab + "_counter" + this.lng_id).is(":checked"), 
+                fuzzy: $("#fs_" + this.currentTab + "_fuzzy" + this.lng_id).is(":checked"), 
+                hideExpandedCounter: $("#fs_" + this.currentTab + "_hideExpandedCounter" + this.lng_id).is(":checked"), 
+                highlight: $("#fs_" + this.currentTab + "_highlight" + this.lng_id).is(":checked"), 
+                mode: $("#fs_" + this.currentTab + "_hideMode" + this.lng_id).is(":checked") ? "hide" : "dimm" 
             },
             init: (event, data) => {
                 let filter = new Filter(this.currentTab, data.tree, this.lng_id);
